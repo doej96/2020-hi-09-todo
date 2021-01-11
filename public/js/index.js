1b2769cbe3d012c89f9790e9e8331fd7fa67ae90
doej96 <doej96@naver.com>
2021-01-11 11:17:27 +0900

    ...

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+/*********** 글로벌 설정 ***********/
+var auth = firebase.auth();
+var google = new firebase.auth.GoogleAuthProvider();
+var facebook = new firebase.auth.FacebookAuthProvider();
+
+/*********** 이벤트 등록 ***********/
+auth.languageCode = 'ko';
+
+
+/*********** 이벤트 콜백 ***********/
+
+
+/*********** 사용자 함수 ***********/
