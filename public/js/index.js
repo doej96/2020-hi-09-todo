aafa16756d6a569a3041827c896baf919a7b0d84
doej96 <doej96@naver.com>
2021-01-11 12:33:27 +0900

    ...

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,43 @@
 /*********** 글로벌 설정 ***********/
 var auth = firebase.auth();
+var db = firebase.database();
+var user = null;
 var google = new firebase.auth.GoogleAuthProvider();
 var facebook = new firebase.auth.FacebookAuthProvider();
 
 /*********** 이벤트 등록 ***********/
 auth.languageCode = 'ko';
+auth.onAuthStateChanged(onAuthChg);
 
+$('#btGoogleLogin').click(onGoogleLogin);
+$('#btLogout').click(onLogout);
 
 /*********** 이벤트 콜백 ***********/
+function onAuthChg(r) {
+	if(r) {
+		user = r;
+		console.log(r);
+		$('.sign-wrap .icon img').attr('src', user.photoURL);
+		$('.sign-wrap .email').html(user.email);
+		$('.modal-wrapper.auth-wrapper').hide();
+		$('#btLogout').show();
+	}
+	else {
+		user = null;
+		$('.sign-wrap .icon img').attr('src', 'http://via.placeholder.com/36');
+		$('.sign-wrap .email').html('');
+		$('.modal-wrapper.auth-wrapper').css('display','flex');
+		$('#btLogout').css('display','none');
+	}
+}
+
+function onGoogleLogin() {
+	auth.signInWithPopup(google);
+}
+
+function onLogout() {
+	auth.signOut();
+}
 
 
 /*********** 사용자 함수 ***********/
