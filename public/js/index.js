d4a1bce88590c285f46d65b48b5ffe3aef4785c3
doej96 <doej96@naver.com>
2021-01-12 14:38:22 +0900

    ...

--- public/js/index.js
+++ public/js/index.js
@@ -14,8 +14,54 @@ function dbInit() {
 }
 
 /*********** 이벤트 콜백 ***********/
+var timeout = null; 
+function onCheck(el) {
+	console.log(el, chk); //i(this)와 check true/false
+	
+	$(el).siblings('i').toggleClass('active');
+	$(el).toggleClass('active');
+
+	if(chk) {
+		timeout = setTimeout(function(){
+			$(el).parent().css('opacity', 0);
+			setTimeout(function(){
+				$(el).parent().remove();
+				db.ref('root/todo/'+user.uid+'/'+$(el).parent().attr('id')).update({
+					checked: true
+				});
+			},750)
+		},3000);
+	}
+	else {
+		clearTimeout(timeout);
+	}
+}
+
+function onSubmit(f) {
+	console.log(f.task.value);
+	var data = {
+		task : f.task.value,
+		createdAt: new Date().getTime(),
+		checked: false,
+	}
+	if(f.task.value !== '') db.ref('root/todo/'+user.uid).push(data);
+	return false;
+}
+
 function onAdd(r) {
-	console.log(r.val());
+	console.log(r.key); //key:고유 아이디
+	console.log(r.val()); //데이터
+	var html = '<li id="'+r.key+'">';
+	html += '	<i class="active far fa-circle" onclick="onCheck(this, true);"></i>';
+	html += '	<i class="far fa-check-circle" onclick="onCheck(this, false);"></i>';
+	html += '	<span>'+r.val().task+'</span>';
+	html += '</li>';
+	var $li = $(html).prependTo($(".list-wrap"));
+	$li.css("opacity"); /* 그냥 주면 transition 안먹음, 한 번 인식시켜준 다음에 css 변경 */
+	$li.css("opacity",1);
+
+	$(".add-wrap")[0].reset(); //reset : form 초기화해주는 자바스크립트 명령
+	//document.querySelector(".add-wrap").reset();
 }
 
 function onRev(r) {
@@ -28,21 +74,22 @@ function onChg(r) {
 
 
 function onAuthChg(r) {
+	user = r;
 	if(r) {
-		user = r;
 		console.log(r);
 		$('.sign-wrap .icon img').attr('src', user.photoURL);
 		$('.sign-wrap .email').html(user.email);
 		$('.modal-wrapper.auth-wrapper').hide();
 		$('#btLogout').show();
+		$('.sign-wrap').show();
 		dbInit();
 	}
 	else {
-		user = null;
 		$('.sign-wrap .icon img').attr('src', 'http://via.placeholder.com/36');
 		$('.sign-wrap .email').html('');
 		$('.modal-wrapper.auth-wrapper').css('display','flex');
 		$('#btLogout').css('display','none');
+		$('.sign-wrap').hide();
 	}
 }
 
