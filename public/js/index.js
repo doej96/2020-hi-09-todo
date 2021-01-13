9867c2eb1c91156b6e44f4f71ead8aab2291891c
doej96 <doej96@naver.com>
2021-01-13 13:10:30 +0900

    오전

--- public/js/index.js
+++ public/js/index.js
@@ -13,7 +13,35 @@ function dbInit() {
 	db.ref('root/todo/'+user.uid).on('child_changed', onChg);
 }
 
+function addHTML(k, v) {
+	var html = '<li id="'+k+'" class="'+(v.checked ? 'opacity':'')+'">';
+	if(v.checked) {
+		html += '	<i class="far fa-circle" onclick="onCheck(this, true);"></i>';
+		html += '	<i class="active far fa-check-circle" onclick="onCheck(this, false);"></i>';
+	} else{
+		html += '	<i class="active far fa-circle" onclick="onCheck(this, true);"></i>';
+		html += '	<i class="far fa-check-circle" onclick="onCheck(this, false);"></i>';
+	}
+	html += '	<input type="text" class="ml-3" value="'+v.task+'" onchange="onChange(\''+k+'\', this);">';
+	html += '	<div class="date">'+moment(v.createdAt).format('llll')+'</div>';
+	html += '<button class="btn btn-sm btn-danger bt-delete" onclick="onDelete">삭제</button>';
+	html += '</li>';
+	
+	var $li = $(html).prependTo($(".list-wrap"));
+	$li.css("opacity"); /* 그냥 주면 transition 안먹음, 한 번 인식시켜준 다음에 css 변경 */
+	$li.css("opacity",1);
+	return $li;
+}
+
 /*********** 이벤트 콜백 ***********/
+function onChange(k, v) {
+	db.ref('root/todo/'+user.uid+'/'+k).update({ task: v.value })
+}
+
+function onDelete(key) {
+	db.ref('root/todo/'+user.uid+'/'+key).remove();
+}
+
 var timeout = null; 
 function onCheck(el, chk) {
 	//console.log(el, chk); //i(this)와 check true/false
@@ -38,7 +66,7 @@ function onCheck(el, chk) {
 
 function onDoneClick() {
 	$('.bt-done').toggleClass('active');
-	var ref = db.ref('note/todo/'+user.uid);
+	var ref = db.ref('root/todo/'+user.uid);
 	if($('.bt-done').hasClass('active')) { //감추기
 		ref.orderByChild('checked').equalTo(false).once('value').then(onGetData);
 		//once:데이터 다 가져옴, then:성공한다면, catch:실패한다면
@@ -49,8 +77,19 @@ function onDoneClick() {
 }
 
 function onGetData(r) {
+	$('.list-wrap').empty();
+	r.forEach(function(v){
+		if(v.val().checked) addHTML(v.key, v.val());
+	});
+	r.forEach(function(v){
+		if(!v.val().checked) addHTML(v.key, v.val());
+	});
+
 	for(var i in r.val()) {
-		console.log(r.val()[i].task);
+		var $li = $(getHTML(i, r.val()[i])).prependTo($(".list-wrap"));
+		//console로 r.val()찍어보면 i가 키값
+		$li.css("opacity");
+		$li.css("opacity",1);
 	}
 }
 
@@ -68,17 +107,7 @@ function onSubmit(f) {
 function onAdd(r) {
 	//console.log(r.key); //key:고유 아이디
 	//console.log(r.val()); //데이터
-	if(!r.val().checked) {
-		var html = '<li id="'+r.key+'">';
-		html += '	<i class="active far fa-circle" onclick="onCheck(this, true);"></i>';
-		html += '	<i class="far fa-check-circle" onclick="onCheck(this, false);"></i>';
-		html += '	<span>'+r.val().task+'</span>';
-		html += '</li>';
-		var $li = $(html).prependTo($(".list-wrap"));
-		$li.css("opacity"); /* 그냥 주면 transition 안먹음, 한 번 인식시켜준 다음에 css 변경 */
-		$li.css("opacity",1);
-	}
-
+	if(!r.val().checked) addHTML(r.key, r.val());
 	$(".add-wrap")[0].reset(); //reset : form 초기화해주는 자바스크립트 명령
 	//document.querySelector(".add-wrap").reset();
 }
@@ -123,6 +152,7 @@ function onLogout() {
 /*********** 이벤트 등록 ***********/
 auth.languageCode = 'ko';
 auth.onAuthStateChanged(onAuthChg);
+moment.locale('ko');
 
 $('#btGoogleLogin').click(onGoogleLogin);
 $('#btLogout').click(onLogout);
