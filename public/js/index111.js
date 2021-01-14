20ee5a9f919686f7cb3b373a537d7632be0458af
doej96@naver.com <doej96@naver.com>
2021-01-14 18:03:45 +0900

    된건가........?

--- public/js/index111.js
+++ public/js/index111.js
@@ -1,15 +1,17 @@
-/*********** 글로벌 설정 ***********/
+/************** 글로벌설정 ***************/
 var auth = firebase.auth();
 var db = firebase.database();
 var storage = firebase.storage();
-var sRef = null;
+var sRef = null; //storage.ref().child('storage');
 var user = null;
 var ref = null;
 var key = null;
 var google = new firebase.auth.GoogleAuthProvider();
 var facebook = new firebase.auth.FacebookAuthProvider();
 
-/*********** 사용자 함수 ***********/
+
+
+/************** 사용자함수 ***************/
 function dbInit() {
 	db.ref('root/todo/'+user.uid).on('child_added', onAdd);
 	db.ref('root/todo/'+user.uid).on('child_removed', toggleList);
@@ -17,40 +19,45 @@ function dbInit() {
 }
 
 function addHTML(k, v) {
-	var html = '<li id="'+k+'" class="'+(v.checked ? 'opacity':'')+'">';
+	var html  = '<li id="'+k+'" class="'+(v.checked ? 'opacity': '')+'">';
 	if(v.checked) {
-		html += '	<i class="far fa-circle" onclick="onCheck(\''+k+'\', true);"></i>';
-		html += '	<i class="active far fa-check-circle" onclick="onCheck(\''+k+'\', false);"></i>';
-	} else{
-		html += '	<i class="active far fa-circle" onclick="onCheck(\''+k+'\', true);"></i>';
-		html += '	<i class="far fa-check-circle" onclick="onCheck(\''+k+'\', false);"></i>';
+		html += '<i class="far fa-circle" onclick="onCheck(\''+k+'\', true);"></i>';
+		html += '<i class="active far fa-check-circle" onclick="onCheck(\''+k+'\', false);"></i>';
 	}
-	html += '	<input type="text" class="ml-3" value="'+v.task+'" onchange="onChange(\''+k+'\', this);" onfocus="onFocus(this);" onblur="onBlur(this);">';
-	//form만 onchange요소 가지고있음
-	html += '	<div class="date">'+moment(v.createdAt).format('llll')+'</div>';
-	html += '<button class="btn btn-sm btn-danger bt-delete" onclick="onDelete(\''+k+'\')">삭제</button>';
+	else {
+		html += '<i class="active far fa-circle" onclick="onCheck(\''+k+'\', true);"></i>';
+		html += '<i class="far fa-check-circle" onclick="onCheck(\''+k+'\', false);"></i>';
+	}
+	html += '<input type="text" class="ml-3" value="'+v.task+'" onchange="onChange(\''+k+'\', this);" onfocus="onFocus(this);" onblur="onBlur(this);">';
+	html += '<div class="date">'+moment(v.createdAt).format('llll')+'</div>';
+	html += '<button class="btn btn-sm btn-danger bt-delete" onclick="onDelete(\''+k+'\');">삭제</button>';
 	html += '</li>';
-	
+
 	var $li = $(html).prependTo($(".list-wrap"));
-	$li.css("opacity"); /* 그냥 주면 transition 안먹음, 한 번 인식시켜준 다음에 css 변경 */
-	$li.css("opacity",1);
+	$li.css("opacity");
+	$li.css("opacity", 1);
+
 	return $li;
 }
 
 function toggleList() {
 	var ref = db.ref('root/todo/'+user.uid);
-	if( $('.bt-done').hasClass('active')) { //감추기
+	if( $('.bt-done').hasClass('active') ) { //감추기
 		ref.orderByChild('checked').equalTo(false).once('value').then(onGetData);
 	}
-	else {
+	else {	//보이기
 		ref.once('value').then(onGetData);
 	}
 }
 
+
 function createFile(name) {
 	var YMD = moment().format('YYYYMMDD');
 	sRef = storage.ref().child('storage/'+YMD);
-	return YMD+'-'+new Date().getTime()+'_'+name;
+	var file = YMD+'-'+new Date().getTime()+name;
+	// 20210114-timestamp-Math.random().jpg
+
+	return { saveName: file, oriName: name };
 }
 
 function isExt(name) {
@@ -61,11 +68,11 @@ function isExt(name) {
 
 function isImg(name) {
 	var allowExt = ['jpg', 'jpeg', 'png', 'gif'];
-	var ext = names = name.split('.').pop().toLowerCase();
-	return allowExt.indexOf(ext) > -1 ? true : false;
+	var ext = names = name.split('.').pop().toLowerCase(); //마지막요소 빼냄, 소문자로 만듦(JS -> js)
+	return allowExt.indexOf(ext) > -1 ? true : false;  //indexOf(ext) : ext가 몇번째에 있는지 그 index return, 없으면 -1
 }
 
-/*********** 이벤트 콜백 ***********/
+/************** 이벤트콜백 ***************/
 function onViewImg(el) {
 	var src = $(el).attr('src');
 	$('.img-wrapper.modal-wrapper img').attr('src', src);
@@ -76,19 +83,16 @@ function onFileDelete(el) {
 	var key = $(el).data('key');
 	var file = $(el).data('file');
 	if(confirm("정말로 삭제하시겠습니까?")) {
-		db.ref('root/todo/'+user.uid+'/'+key).update({file: null});
-		storage.ref().child('storage/'+file.split('_')[0]+'/'+file).delete().then(function(){
-			$('.edit-wrapper .file-wrap').css('display', 'none');
-		});
+		db.ref('root/todo/'+user.uid+'/'+key).update({file: null, oriFile: null});
+		storage.ref().child('storage/'+file.split('_')[0]+'/'+file).delete().then();
 	}
 }
 
 function onReset(f) {
 	f.key.value = '';
-	f.reset();
 	$('.edit-wrapper').find('button.btn-primary').removeClass('d-none');
 	$('.edit-wrapper').find('button.btn-success').addClass('d-none');
-	$('.edit-wrapper .file-wrap').css('display', 'none');
+	$('.edit-wrapper .file-wrap').css("display", "none");
 }
 
 function onGetTask(r) {
@@ -106,30 +110,28 @@ function onGetTask(r) {
 			$('.edit-wrapper .file-wrap .image').hide();
 			$('.edit-wrapper .file-wrap .pds').attr('href', r.val().file.url).html(r.val().file.oriName).show();
 		}
-	}
-	else $('.edit-wrapper .file-wrap').css('display', 'none');
+	} 
+	else $('.edit-wrapper .file-wrap').css("display", "none");
 	$('.edit-wrapper').find('button.btn-primary').addClass('d-none');
 	$('.edit-wrapper').find('button.btn-success').removeClass('d-none');
 }
 
+
+
 function onEdit(f) {
 	//파일업로드 처리
-	//파일은 storage에 파일주소는 realtime database에
-	if(f.upfile.files[0]) {
-		var file = f.upfile.files[0]; //files : input type=file 만 가지고 있는 객체
-		if(isExt(file.name)) {
-			var saveName = createFile(file.name);
-			var fRef = sRef.child(saveName);
-			fRef.put(file).on('state_changed', onProgress, onError, onUploaded);
-		}
-		else {
-			alert("선택한 파일은 업로드할 수 없습니다.");
-			return false;
-		}
+	// console.log(file);
+	// console.log(file[0].name);
+	var file = f.upfile.files[0];
+	if(isExt(file.name)) {
+	  var upload = createFile(file.name);
+	  var fRef = sRef.child(upload.saveFile);
+	  fRef.put(file).on('state_changed', onProgress, onError, onUploaded);
 	}
-	else onSave();
-
-	//file 콜백 (함수 내 콜백)
+	else {
+		alert(file.name+"은(는) 업로드할 수 없습니다.")
+	}
+	return false;
 	function onProgress(r) {
 		console.log(r);
 	}
@@ -137,33 +139,30 @@ function onEdit(f) {
 		console.log(e);
 	}
 	function onUploaded() {
-		fRef.getDownloadURL().then(onSave);
+		fRef.getDownloadURL().then(function(url) {
+			$('#img').attr('src', url);
+			console.log('File available at', url);
+		});
 	}
-
-		function onSave(url) {
-			var key = f.key.value;
-			var data = {
-				task: f.task.value,
-				comment: f.comment.value,
-				createdAt: new Date().getTime(),
-				checked: false,
-				};
-			if(url) {
-				data.file = {};
-				data.file.url = url;
-				data.file.oriName = file.name;
-				data.file.saveName = saveName;
-			}
-			if(key == "") {
-				db.ref('root/todo/'+user.uid).push(data);
-			}
-			else {
-				db.ref('root/todo/'+user.uid+'/'+key).update(data);
-			}
-			f.key.vlaue = '';
-			f.reset();
-		}
-		return false;
+	/*
+	var key = f.key.value;
+	var data = { 
+		task: f.task.value, 
+		comment: f.comment.value, 
+		createdAt: new Date().getTime(), 
+		checked: false,
+		file:  
+	};
+	if(key == "") {
+		db.ref('root/todo/'+user.uid).push(data);
+	}
+	else {
+		db.ref('root/todo/'+user.uid+'/'+key).update(data);
+	}
+	f.key.value = '';
+	f.reset();
+	*/
+	return false;
 }
 
 function onFocus(el) {
@@ -181,7 +180,7 @@ function onKeyup(el) {
 }
 
 function onChange(k, v) {
-	db.ref('root/todo/'+user.uid+'/'+k).update({ task: v.value })
+	db.ref('root/todo/'+user.uid+'/'+k).update({ task: v.value });
 }
 
 function onDelete(key) {
@@ -190,24 +189,23 @@ function onDelete(key) {
 
 function onCheck(key, chk) {
 	db.ref('root/todo/'+user.uid+'/'+key).update({ checked: chk });
-	/* 
-	$(el).siblings('i').toggleClass('active');
-	$(el).toggleClass('active');
+	/*
+	$(el).siblings('i').addClass('active');
+	$(el).removeClass('active');
 	if(chk) {
-		timeout = setTimeout(function(){
+		timeout = setTimeout(function(){ 
 			$(el).parent().css('opacity', 0);
 			setTimeout(function(){
+				var data = { checked: true };
 				$(el).parent().remove();
-				db.ref('root/todo/'+user.uid+'/'+$(el).parent().attr('id')).update({
-					checked: true
-				});
-			},750)
-		},3000);
+				db.ref('root/todo/'+user.uid+'/'+$(el).parent().attr('id')).update(data)
+			}, 750) 
+		}, 3000);
 	}
 	else {
 		clearTimeout(timeout);
 	}
-	 */
+	*/
 }
 
 function onDoneClick() {
@@ -217,18 +215,17 @@ function onDoneClick() {
 
 function onGetData(r) {
 	$('.list-wrap').empty();
-	r.forEach(function(v) {
+	r.forEach(function(v){
 		if(v.val().checked) addHTML(v.key, v.val());
 	});
-	r.forEach(function(v) {
+	r.forEach(function(v){
 		if(!v.val().checked) addHTML(v.key, v.val());
-	})
+	});
 }
 
 function onSubmit(f) {
-	console.log(f.task.value);
 	var data = {
-		task : f.task.value,
+		task: f.task.value,
 		createdAt: new Date().getTime(),
 		checked: false,
 	}
@@ -237,29 +234,24 @@ function onSubmit(f) {
 }
 
 function onAdd(r) {
-	//console.log(r.key); //key:고유 아이디
-	//console.log(r.val()); //데이터
 	if(!r.val().checked) addHTML(r.key, r.val());
-	$(".add-wrap")[0].reset(); //reset : form 초기화해주는 자바스크립트 명령
-	//document.querySelector(".add-wrap").reset();
+	// $(".add-wrap")[0].reset();
+	document.querySelector(".add-wrap").reset();
 }
 
 function onAuthChg(r) {
 	user = r;
 	if(r) {
-		console.log(r);
 		$('.sign-wrap .icon img').attr('src', user.photoURL);
 		$('.sign-wrap .email').html(user.email);
 		$('.modal-wrapper.auth-wrapper').hide();
-		$('#btLogout').show();
 		$('.sign-wrap').show();
 		dbInit();
 	}
 	else {
-		$('.sign-wrap .icon img').attr('src', 'http://via.placeholder.com/36');
+		$('.sign-wrap .icon img').attr('src', 'https://via.placeholder.com/36');
 		$('.sign-wrap .email').html('');
-		$('.modal-wrapper.auth-wrapper').css('display','flex');
-		$('#btLogout').css('display','none');
+		$('.modal-wrapper.auth-wrapper').show();
 		$('.sign-wrap').hide();
 	}
 }
@@ -270,32 +262,21 @@ function onGoogleLogin() {
 
 function onLogout() {
 	auth.signOut();
-	$(".list-wrapper .list-wrap").empty();
-	$(".edit").empty();
-	onReset($(".edit-wrapper form")[0]);
 }
 
-function onListToggle() {
-	$(this).toggleClass('active');
-	if($(this).hasClass('active')) {
-		$('.list-wrapper').removeClass('active');
-	}else {
-		$('.list-wrapper').addClass('active');
-	}
-}
 
-/*********** 이벤트 등록 ***********/
+
+
+/************** 이벤트등록 ***************/
 auth.languageCode = 'ko';
 auth.onAuthStateChanged(onAuthChg);
 moment.locale('ko');
 
 $('#btGoogleLogin').click(onGoogleLogin);
 $('#btLogout').click(onLogout);
-$('.img-wrapper.modal-wrapper img').click(function(e){
+$('.img-wrapper.modal-wrapper img').click(function(){
 	e.stopPropagation();
 });
 $('.img-wrapper.modal-wrapper').click(function(){
 	$(this).css('display', 'none');
 });
-
-$('.list-wrapper .bt-close').click(onListToggle);
